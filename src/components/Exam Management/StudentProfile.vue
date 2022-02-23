13fe55e34e38b600e5e83385ae493b347b6055f7
Jisan-mia <jisanmia47@gmail.com>
2022-02-23 21:41:29 +0600

    fixed

--- src/components/Exam Management/StudentProfile.vue
+++ src/components/Exam Management/StudentProfile.vue
@@ -160,7 +160,7 @@ export default {
         const mainScore = Number(report.score) + Number(report.negative_marking)
         const sM = (mainScore/Number(report.total_mark)) * 100
         const scorePercent = Math.round(sM*100)/100
-        console.log(mainScore, sM, scorePercent)
+        // console.log(mainScore, sM, scorePercent)
 
         return {
           exam_name: report.Exam_name,
